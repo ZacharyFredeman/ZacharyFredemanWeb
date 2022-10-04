ddb25b1afb98df4acd95b5f7a69e87eb8bc13243
ZacharyFredeman <zachary.fredeman1@marist.edu>
2022-10-04 16:05:30 -0400

    gitingonrre
    added gitignore

---
+++ src/componets/Navbar.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react'
+
+function Navbar(){
+    return(
+        <section>
+            <nav className ="navbar">
+            
+            </nav>
+        </section>
+    )
+}
